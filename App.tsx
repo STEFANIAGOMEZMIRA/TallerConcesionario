764c8eb589b82ca9e2bfe04e0160f1199cd7039f
STEFANIAGOMEZMIRA <gomezstefania030@gmail.com>
2024-03-16 09:17:04 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+import Menu from './src/component/Menu'; 
+import Contact from './src/component/Contact';
+
+const App = () => {
+  return (
+    <View>
+      <Menu />
+    </View>
+  );
+};
+
+/*const App = () => {
+  return (
+    <View>
+      <Contact />
+    </View>
+  );
+};*/
+
+
+
+
+export default App;
